4157c777a08d7ed8b2e1227cbb76b74df21ddcfe
Babar-Meet <babarmeet677@gmail.com>
2025-07-28 20:02:35 +0530

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,5 +5,11 @@ pip install PyQt5 yt-dlp requests
 REM Add to system PATH
 setx PATH "%PATH%;%~dp0dist"
 
+pip install pyinstaller
+pyinstaller --onefile --windowed ^
+--add-data "youtube.com_cookies.txt;." ^
+--name VideoDownloader ^
+Vidoedownlaoder.py
+
 echo Installation complete! Use 'VideoDownloader' command to run.
 pause
